21222110d43c7193cd6179bf861d3f7fa568bf02
Christopher Wallis <christopher.j.wallis@gmail.com>
2018-06-14 08:02:37 -0400

    changed output directory for github

---
+++ docs-src/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'Scroll Out',
+    description: 'Animate on Scroll',
+    dest: "./docs",  
+    head: [
+      ['link', { rel: 'icon', href: '/favicon.png' }]
+    ],
+    themeConfig: {
+      nav: []
+    }
+}
--- docs/.vuepress/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    title: 'Scroll Out',
-    description: 'Animate on Scroll'
-}
